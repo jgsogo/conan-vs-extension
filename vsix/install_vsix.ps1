7b7b7ce162d9eba4bc0d63d0f91a3298e6af3384
Javier G. Sogo <jgsogo@gmail.com>
2019-08-01 16:12:48 +0200

    typo

--- vsix/install_vsix.ps1
+++ vsix/install_vsix.ps1
@@ -6,7 +6,7 @@ Write-Host "visualStudioInstallation: $visualStudioInstallation"
 Write-Host "vsixInstaller: $vsixInstaller"
 Write-Host "localArtifactPath: ${env:localArtifactPath}"
 
-Start-Process -FilePath "$vsixInstaller" -ArgumentList "/q /a /sp /logFile:\"C:\projects\install.log\" ${env:localArtifactPath}" -Wait -PassThru;
+Start-Process -FilePath "$vsixInstaller" -ArgumentList "/q /a /sp /logFile:C:\projects\install.log ${env:localArtifactPath}" -Wait -PassThru;
 
 $content = Get-Content -Path C:\projects\install.log
 Write-Host "log output: $content"
